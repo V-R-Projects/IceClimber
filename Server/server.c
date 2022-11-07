c278744db6217d32fb00ad5d1bbdc324dc9ae5e3
valesskablanco <valesskablanco@estudiantec.cr>
2022-11-06 20:48:03 -0600

    initial files

---
+++ Server/server.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <json/json.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+void server_comms()
+{
+    int listenfd = 0, connfd = 0;
+}
