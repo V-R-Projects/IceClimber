abe5d1d4ed5671bad8ef32c6b0e5b49922eda80d
valesskablanco <valesskablanco@estudiantec.cr>
2022-11-06 21:59:39 -0600

    sending strings

--- Server/server.c
+++ Server/server.c
@@ -1,14 +1,77 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <json/json.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-void server_comms()
+#define SIZE 1024
+#define IP_ADDR "127.0.0.1"
+
+int main(void)
 {
-    int listenfd = 0, connfd = 0;
+    int socket_desc, client_sock, client_size;
+    struct sockaddr_in server_addr, client_addr;
+    char server_message[SIZE], client_message[SIZE];
+    
+    // Clean buffers:
+    memset(server_message, '\0', sizeof(server_message));
+    memset(client_message, '\0', sizeof(client_message));
+    
+    // Create socket:
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if(socket_desc < 0){
+        printf("Error while creating socket\n");
+        return -1;
+    }
+    printf("Socket created successfully\n");
+    
+    // Set port and IP:
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SIZE);
+    server_addr.sin_addr.s_addr = inet_addr(IP_ADDR);
+    
+    // Bind to the set port and IP:
+    if(bind(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr))<0){
+        printf("Couldn't bind to the port\n");
+        return -1;
+    }
+    printf("Done with binding\n");
+    
+    // Listen for clients:
+    if(listen(socket_desc, 1) < 0){
+        printf("Error while listening\n");
+        return -1;
+    }
+    printf("\nListening for incoming connections.....\n");
+    
+    // Accept an incoming connection:
+    client_size = sizeof(client_addr);
+    client_sock = accept(socket_desc, (struct sockaddr*)&client_addr, &client_size);
+    
+    if (client_sock < 0){
+        printf("Can't accept\n");
+        return -1;
+    }
+    printf("Client connected at IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    
+    // Receive client's message:
+    if (recv(client_sock, client_message, sizeof(client_message), 0) < 0){
+        printf("Couldn't receive\n");
+        return -1;
+    }
+    printf("Msg from client: %s\n", client_message);
+    
+    // Respond to client:
+    strcpy(server_message, "This is the server's message.");
+    
+    if (send(client_sock, server_message, strlen(server_message), 0) < 0){
+        printf("Can't send\n");
+        return -1;
+    }
+    
+    // Closing the socket:
+    close(client_sock);
+    close(socket_desc);
+    
+    return 0;
 }
