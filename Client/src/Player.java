bf1276a9c391d71abe9350ff060716838f2c0812
valesskablanco <valesskablanco@estudiantec.cr>
2022-11-19 22:36:13 -0600

    player class

--- Client/src/Player.java
+++ Client/src/Player.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+
 public class Player {
+
+    private String spritePath;
+    private int speed = 5;
+    private ArrayList<Integer> position;
+    private int mode = 0; // 0 is not in attack mode, 1 is in attack mode
+
+    public Player(String player, ArrayList initialPosition) {
+        if (player == "Popo") {
+            spritePath = "./sprites/Popo.jpg";
+        }
+        else if (player == "Nana") {
+            spritePath = "./sprites/Nana.jpg";
+        }
+        position = initialPosition;
+    }
+    public void moveRight() {
+        position.set(0, (position.get(0) + speed));
+    }
+    public void moveLeft() {
+        position.set(0, (position.get(0) - speed));
+    }
+    public void jump() {
+        position.set(1, (position.get(1) + speed));
+    }
+    public void fall() {
+        position.set(1, (position.get(1) - speed));
+    }
+
+    public ArrayList getPosition() {
+        return position;
+    }
 }
